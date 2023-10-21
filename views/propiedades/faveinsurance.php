1a8c623801a322ec9a9ec2d547c64c09b1dbebcc
VieraxD <vieraalfredoj@gmail.com>
2023-10-21 01:41:34 -0500

    first commit

--- views/propiedades/faveinsurance.php
+++ views/propiedades/faveinsurance.php
@@ -1,65 +1,69 @@
 
 <?php include_once __DIR__ . "/../dashboard/header.php"; ?>
-    <section class="slider">
+
+
+<section>
     
-    <div class="slider-container">
-   
-    <div class="slide active">
+  <div class="slider">
+          <div class="slider-container">
+              
+                <div class="slide active">
 
-        <picture class="picture" >
-          <img class="familia" loading="lazy" src="build/img/familia2.png" alt="imagen de familia">
-        </picture>
-          <span class="animated bounceInLeft  title-1">Cobertura Integral de Salud</span>
-          <p class="animated bounceInRight  texto-1">Atención médica, hospitalización y medicamentos recetados</p>      
-    </div>
-        
-        <div
-        class="slide"
-        >
-        <picture class="picture" >
-          <img class="sonrisa" loading="lazy"  src="build/img/sonrisa.png" alt="imagen de sonrisa">
-        </picture><span class="animated bounceInDown title-2">Sonrisas Saludables <br>Garantizadas</span>
-          <p class="animated bounceInUp  texto-2">Nuestro plan dental cubre limpiezas regulares, tratamientos y cirugía dental a precios asequibles</p>  
-      </div>
+                    <picture class="picture" >
+                      <img class="familia" loading="lazy" src="build/img/familia2.png" alt="imagen de familia">
+                    </picture>
+                      <span class="title-1">Cobertura Integral de Salud</span>
+                      <p class="texto-1">Atención médica, hospitalización y medicamentos recetados</p>      
+                </div>
+              
+              <div class="slide">
+              <picture class="picture" >
+                <img class="sonrisa" loading="lazy"  src="build/img/sonrisa.png" alt="imagen de sonrisa">
+              </picture><span class="title-2">Sonrisas Saludables <br>Garantizadas</span>
+                <p class="texto-2">Nuestro plan dental cubre limpiezas regulares, tratamientos y cirugía dental a precios asequibles</p>  
+            </div>
 
-      <div
-        class="slide"
-        ><picture class="picture" >
-          <img class="vision" loading="lazy"src="build/img/vision1.png" alt="imagen de vision">
-        </picture>
-        
-        <span class="title-3">Cuidado Visual Premium</span>
-         
-      </div>
+            <div
+              class="slide"
+              ><picture class="picture" >
+                <img class="vision" loading="lazy"src="build/img/vision1.png" alt="imagen de vision">
+              </picture>
+              
+              <span class="title-3">Cuidado Visual Premium</span>
+              
+            </div>
 
-      <div
-        class="slide"
-        >
-        <picture class="picture" >
-          <img class="doctores" loading="lazy"  src="build/img/medicos1.png" alt="imagen de doctores">
-        </picture>
-        <span class="title-4">Detección Temprana <br>Esperanza Duradera</span>
-          <p class="texto-4">Nuestra cobertura de diagnóstico de cáncer te brinda tranquilidad, con pruebas de detección temprana y tratamiento de vanguardia</p>  </div>
+            <div
+              class="slide"
+              >
+              <picture class="picture" >
+                <img class="doctores" loading="lazy"  src="build/img/medicos1.png" alt="imagen de doctores">
+              </picture>
+              <span class="title-4">Detección Temprana <br>Esperanza Duradera</span>
+                <p class="texto-4">Nuestra cobertura de diagnóstico de cáncer te brinda tranquilidad, con pruebas de detección temprana y tratamiento de vanguardia</p>  
+              </div>
 
-    
+          
 
-        <button class="arrow left-arrow" id="left">
-        <i class="fas fa-arrow-left"></i>
-        </button>
+              <button class="arrow left-arrow" id="left">
+              <i class="fas fa-arrow-left"></i>
+              </button>
 
-        <button class="arrow right-arrow" id="right">
-            <i class="fas fa-arrow-right"></i>
-        </button>
+              <button class="arrow right-arrow" id="right">
+                  <i class="fas fa-arrow-right"></i>
+              </button>
 
-        <div class="slide-indicators">
-          <div class="indicator active"></div>
-          <div class="indicator"></div>
-          <div class="indicator"></div>
-          <div class="indicator"></div>
-         
-        </div>
+              <div class="slide-indicators">
+                <div class="indicator active"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+              
+              </div>
 
 
+
+          </div>
 
     </div>
 
@@ -78,9 +82,11 @@
               </div>
               <div class="servicio-body">
                   <p class="servicio-title">Seguros de Salud</p>
-                  <a href="#"><p class="servicio-description">contáctanos</p></a>
-                
               </div>
+              <div class="descripcion">
+                  <a href="#"><p class="servicio-description">contáctanos</p></a>
+              </div>
+                
           </div>
 
           <div class="servicio">
@@ -89,6 +95,8 @@
               </div>
               <div class="servicio-body">
                   <p class="servicio-title">Seguros de Vida</p>
+                  </div>
+              <div class="descripcion">
                   <a href="#"><p class="servicio-description">contáctanos</p></a>
                 
               </div>
@@ -101,6 +109,8 @@
               </div>
               <div class="servicio-body">
                   <p class="servicio-title">Seguros de visión</p>
+              </div>
+              <div class="descripcion">
                   <a href="#"><p class="servicio-description">contáctanos</p></a>
                 
               </div>
@@ -113,6 +123,8 @@
               </div>
               <div class="servicio-body">
                   <p class="servicio-title">Seguros dentales</p>
+              </div>
+              <div class="descripcion">
                   <a href="#"><p class="servicio-description">contáctanos</p></a>
                 
               </div>
@@ -125,6 +137,8 @@
               </div>
               <div class="servicio-body">
                   <p class="servicio-title">Seguros diagnostico de cancer</p>
+                </div>
+              <div class="descripcion">
                   <a href="#"><p class="servicio-description">contáctanos</p></a>
                 
               </div>
@@ -133,6 +147,45 @@
     </section>
 
     <!-- Servicios -->
+
+
+
+     <!-- Quienes Somos -->
+
+      <section>
+          
+      
+          <div class="container-nosotros">
+
+             <div class="nosotros">
+
+
+                <div class="nosotros-row">
+
+                        <div class="nosotros-contenedor">
+                          <div class="nosotros-t">
+                            <a href="#"><p class="nosotros-title">Bienvenido a Fave Insurance</p></a>
+                          </div>
+                          <div >
+                            <p class="nosotros-description" >Sabemos que cada cliente tiene sus propias necesidades, por eso analizamos cada caso de forma independiente valorando las necesidades y las soluciones disponibles en el mercado asegurador</p>
+                          </div>
+                          <div class="separador-horizontal"></div>
+                        </div>
+
+                </div>   
+              
+            </div>
+
+        
+          </div>
+
+
+      </section>
+
+
+
+      <!-- Quienes Somos -->
+
 
 
     <!-- Informaciones -->
@@ -145,16 +198,19 @@
             <div class="informacion">
                 
                 <div class="informacion-header">
-                    <img class="foto6" loading="lazy" src="build/img/foto6.jpeg" alt="Representante de seguro">
+                    <img class="foto6" loading="lazy" src="build/img/segurodevida.jpg" alt="Representante de seguro">
                 </div>
 
-                  <div class="informacion-body">
-                    <h3 class="informacion-titulo">¿Qué es un seguro de <br>vida?</h3> 
+                  <div class="informacion-titulo">
+                    
+                    <h3 class="informacion-t">¿Qué es un seguro de <br>vida?</h3>
+                  </div> 
+                  <div class="informacion-parrafo">
                     <p class="informacion-p">Un seguro de vida es un contrato entre el asegurado y la compañía de seguros en el que se paga una prima a cambio de una suma de dinero que se otorga a los beneficiarios designados en caso de fallecimiento del asegurado.</p>
                   </div>
             </div>
             
-            <div class="informacion">
+            <div class="informacion-S">
 
               <div class="separador"></div>
             </div>
@@ -162,34 +218,39 @@
             <div class="informacion">
                 
                 <div class="informacion-header">
-                    <img class="foto6" loading="lazy" src="build/img/foto5.jpeg" alt="Representante de seguro">
+                    <img class="foto6" loading="lazy" src="build/img/segurodesalud.jpg" alt="Representante de seguro">
                 </div>
 
-                  <div class="informacion-body">
-                    <h3 class="informacion-titulo">¿Qué es un seguro de <br>salud?</h3> 
+                  <div class="informacion-titulo">
+                    <h3 class="informacion-t">¿Qué es un seguro de <br>salud?</h3> 
+                    </div> 
+                  <div class="informacion-parrafo">
                     <p class="informacion-p">Un seguro de salud es un contrato entre el asegurado y la compañía de seguros en el que se paga una prima a cambio de cobertura médica y hospitalaria en caso de enfermedad o lesión.</p>
                   </div>
             </div>
 
-      </div>
+            <div class="informacion-S">
 
+            <div class="separador"></div>
+            </div>
 
-      <div class="contenedor-informacion">
 
 
             <div class="informacion">
                 
                 <div class="informacion-header">
-                      <img class="foto6" loading="lazy" src="build/img/foto1.jpeg" alt="Representante de seguro">
+                      <img class="foto6" loading="lazy" src="build/img/dentistas.jpg" alt="Representante de seguro">
                 </div>
 
-                  <div class="informacion-body">
-                    <h3 class="informacion-titulo">¿Qué son los seguros <br>complementarios?</h3>
+                <div class="informacion-titulo">
+                    <h3 class="informacion-t">¿Qué son los seguros <br>complementarios?</h3>
+                    </div> 
+                  <div class="informacion-parrafo">
                     <p class="informacion-p">Los seguros complementarios son aquellos que se contratan adicionalmente al seguro principal para cubrir gastos específicos que no están incluidos en la cobertura básica, como por ejemplo, seguros dentales o de visión.</p>
                   </div>
             </div>
 
-            <div class="informacion">
+            <div class="informacion-S">
 
             <div class="separador"></div>
             </div>
@@ -198,82 +259,127 @@
             <div class="informacion">
                 
                 <div class="informacion-header">
-                      <img class="foto6" loading="lazy" src="build/img/foto2.jpeg" alt="Representante de seguro">
+                      <img class="foto6" loading="lazy" src="build/img/seguronecesitas.jpg" alt="Representante de seguro">
                 </div>
 
-                  <div class="informacion-body">
-                    <h3 class="informacion-titulo">¿Que seguro necesitas?</h3>
+                <div class="informacion-titulo">
+                    <h3 class="informacion-t">¿Que seguro necesitas?</h3>
+                    </div> 
+                  <div class="informacion-parrafo">
                     <p class="informacion-p">Seguros de vida, salud y protección personal a medida. Cuidamos lo que más importa: tú y tus seres queridos. Adapta tu cobertura a la vida que vives</p>
                   </div>
             </div>
 
-    </div>
+
+
+      </div>
+
 
 </section>
 
 <!-- Informaciones -->
+
+<!-- Asesores -->
+
+  <section>
+
+      <div class="contenedor-asesores">
+
+        <div class="asesores-titulos">
+          <div class="titulos">
+            <h2 class="titulo">Protege lo que más te importa</h3>
+          </div>
+        </div>
+
+        <div class="contenedor-asesor">
+
+            <div class="asesores">
+
+                <div class="asesor">
+                      <img class="" loading="lazy" src="build/img/image2.jpeg" alt="Representante de seguro">
+                </div>
+
+            </div>
+
+            <div class="separador-A">
+
+                <div class="asesor-separador">
+                    
+                </div>
+                
+            </div>
+
+
+            <div class="asesores">
+
+                <div class="asesor">
+                      <img class="" loading="lazy" src="build/img/image1.jpeg" alt="Representante de seguro">
+                </div>
+                
+            </div>
+        </div>
+
+      </div>
+
+
+
+  </section>
+
+
+
+<!-- Asesores -->
 
 
 <!-- Atencion -->
 
 <section class="atenciones">
 
+    <div class="contenedor-atencion">
 
-
-        <div class="contenedor-atencion">
-
-              <div class="atencion">
-                  <div class="atencion-header">
-                      <a href="#"> <img class="logo-atencion " loading="lazy" src="build/img/logoSS.png" alt="logo"></a>
-                  </div>
-                  <div class="atencion-body">
-                      <a href="#"><p class="atencion-title">Bienvenido a Fave Insurance</p></a>
-                      <p class="atencion-description">Sabemos que cada cliente tiene sus propias necesidades, por eso analizamos cada caso de forma independiente valorando las necesidades y las soluciones disponibles en el mercado asegurador</p>
-                    
-                  </div>
-             
+          <div class="atencion">
+              <div class="atencion-header">
+                    <a href="#"> <img class="tellamamos" loading="lazy" src="build/img/tellamamosS.png" alt="tellamamos"></a>
               </div>
-
-
-              <div class="atencion">
-                  <div class="atencion-header">
-                        <a href="#"> <img class="cobertura" loading="lazy" src="build/img/coberturaS.png" alt="cobertura"></a>
-                  </div>
-                  <div class="atencion-body">
-                      <a href="#"><p class="atencion-title">Cobertura a Tu Medida</p></a>
-                      <p class="atencion-description">Seleccionamos la póliza óptima para ti, maximizando la cobertura al precio más competitivo.</p>
-                    
-                  </div>
-             
+              <div class="atencion-t">
+                  <a href="#"><p class="atencion-title">Cobertura a Tu Medida</p></a>
               </div>
-
-
-
-              <div class="atencion">
-                  <div class="atencion-header">
-                        <a href="#"> <img class="tellamamos" loading="lazy" src="build/img/tellamamosS.png" alt="tellamamos"></a>
-                  </div>
-                  <div class="atencion-body">
-                      <a href="#"><p class="atencion-title">Te llamamos</p></a>
-                      <p class="atencion-description">Deja tus datos y recibirás una llamada de nuestro equipo en breve.</p>
-                  </div>
-             
+              <div class="atencion-d">
+                  <p class="atencion-description">Seleccionamos la póliza óptima para ti, maximizando la cobertura al precio más competitivo.</p>
               </div>
+                
+          </div>
 
-              <div class="atencion">
-                  <div class="atencion-header">
-                      <a href="#"><img class="atencion atencion-picture" loading="lazy" src="build/img/atencionS.png" alt="atencion"></a>
-                  </div>
-                  <div class="atencion-body">
-                      <a href="#"><p class="atencion-title">Atención personalizada 24/7</p></a>
-                      <p class="atencion-description">Desde el primer momento tras un siniestro, gestionamos y te asesoramos en todo el proceso.</p>
-                  </div>
-             
+
+
+          <div class="atencion">
+              <div class="servicio-header">
+                  <a href="#"> <img class="cobertura" loading="lazy" src="build/img/coberturaS.png" alt="cobertura"></a>
               </div>
+              <div class="atencion-t">
+                  <a href="#"><p class="atencion-title">Te llamamos</p></a>
+              </div>
+              <div class="atencion-d">
+                  <p class="atencion-description">Deja tus datos y recibirás una llamada de nuestro equipo en breve.</p>
+              </div>
+                
+          </div>
 
+          <div class="atencion">
+              <div class="servicio-header">
+                 <a href="#"><img class="atencion atencion-picture" loading="lazy" src="build/img/atencionS.png" alt="atencion"></a>
+              </div>
+              <div class="atencion-t">
+                    <a href="#"><p class="atencion-title">Atención personalizada 24/7</p></a>
+              </div>
+              <div class="atencion-d">
+                  <p class="atencion-description">Desde el primer momento tras un siniestro, gestionamos y te asesoramos en todo el proceso.</p>
+              </div>
+                
+          </div>
 
-
-        </div>
+          
+    </div>
+ 
 
 </section>
 
